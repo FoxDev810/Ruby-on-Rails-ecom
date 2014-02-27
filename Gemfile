openstreetmap/openstreetmap-website

Use Konacha for testing JavaScript

Gemfile
@@ -70,6 +70,11 @@ group :test do
   gem 'minitest', '~> 4.7.0', :platforms => [:ruby_19, :ruby_20]
 end
 
+group :test, :development do
+  gem 'konacha'
+  gem 'poltergeist'
+end
+
 # Gems needed for compiling assets
 group :assets do
   gem 'sass-rails', '~> 3.2.3'

FoxDev810/Ruby-on-Rails-ecom

Gemfile
@@ -86,3 +86,9 @@ group :test do
   gem 'timecop'
   gem 'minitest', '~> 4.7.0', :platforms => [:ruby_19, :ruby_20]
 end
+
+# Needed in development as well so rake can see konacha tasks
+group :development, :test do
+  gem 'konacha'
+  gem 'poltergeist'
+end